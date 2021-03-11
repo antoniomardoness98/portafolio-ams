ba9bab3a264b03eccf545e380ffe1fdb38a1c839
Antonio Mardones <antonio.mardones98@gmail.com>
2021-03-11 20:16:22 -0300

    'finalizando'

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,6 +7,8 @@ const About = () => {
     <div className='about-container' id='aboutme'>
       <div className="about-desc">
         <h3> <b>Te contaré un poco sobre mi!</b> </h3>
+        <hr/>
+        <br/>
         <p>Mi nombre es: Antonio Mardones San Martin, actualmente soy estudiante de Ingenieria en Ejecución y computación informatica, tengo 23 años, soy   Chileno viviendo actualmente en la octava región.
           Mis mayores hobbies son el desarrollo web y los video-juegos!
         </p>
@@ -15,7 +17,7 @@ const About = () => {
         <p>pd: Sus lolcitos?</p>
       </div>
       <div className="about-img">
-        <img src={imgGato} alt="about" height='500px' />
+        <img src={imgGato} alt="about" height='500px' width='100%' />
       </div>
     </div>
   )
